3a10ba3fc8fd6975a29534105ba95957e6db0406
ncarrillo93 <nicolascarrillo33@gmail.com>
2020-11-30 01:01:38 -0300

    Tabla lista

--- Tarea 1 LogisticRegression/Tarea_Logistic_Regression.py
+++ Tarea 1 LogisticRegression/Tarea_Logistic_Regression.py
@@ -1,10 +1,16 @@
 # Importar la info
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import datasets
+
 cancer = datasets.load_breast_cancer()
-print('Tipos de tumor: ',list(cancer.target_names))
 X = cancer.data
 y = cancer.target
-
+print('-------------------------------------------------')
+print('Tipos de tumor: ',list(cancer.target_names))
+print('Caracteristicas de tumor: ')
+print(list(cancer.feature_names))
+print('-------------------------------------------------')
 #Separar datos de entrenamiento y prueba.
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=1)
@@ -17,12 +23,40 @@ X_train_std = escalar.transform(X_train) # Realice la estandarización centrando
 X_test_std  = escalar.transform(X_test)
 
 # Definicion del algoritmo a utilizar "Logistic Regression"
+from matplotlib.colors import ListedColormap
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from tabulate import tabulate
 import warnings
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02) :
+    plt.figure(figsize=(14,10))
+    #Setup market generator and colormap
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue','lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y)) ] )
+
+    # plot the decision surface
+    x1_min, x1_max = X[:,0].min() -1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:,1].min() -1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+
+    #plot class example
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y== cl,0], y = X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl, edgecolor='black')
+    if test_idx:
+        #plot all examples
+        X_test, y_test, = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:,1], c='', edgecolor='black', alpha=1.0, linewidth=1, marker='o',s=100 ,label='test set')
+
 
 """ solver {'newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'}, predeterminado = 'lbfgs'  Algoritmo a utilizar en el problema de optimización.
 Para conjuntos de datos pequeños, 'liblinear' es una buena opción, mientras que 'sag' y 'saga' son más rápidos para los grandes.
@@ -39,19 +73,25 @@ Para 'multinomial', la pérdida minimizada es el ajuste de pérdida multinomial 
 'auto' selecciona 'ovr' si los datos son binarios, o si solver = 'liblinear', y de lo contrario selecciona 'multinomial'.
  """
 solv   = ['newton-cg', 'liblinear', 'sag', 'saga']
-mclass = ['auto', 'ovr' , 'multinomial']# , 'multinomial'
+mclass = ['multinomial','ovr']# 'auto',
 table = []
 warnings.filterwarnings("ignore")
 
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+i=0
+C =[1,10,50,100,1000,10000,100000]
 for s in solv:
     for mc in mclass:
-        if mc=='multinomial':
-            if s=='liblinear':
-                break
-        lr = LogisticRegression(C=100, random_state =1,solver=s,multi_class=mc)
-        lr.fit(X_train, y_train)
-        y_pred = lr.predict(X_test)
-        #Verificacion
-        table.append([s,mc,"{:.3f}".format(precision_score(y_test, y_pred)*100),"{:.3f}".format(accuracy_score(y_test,y_pred)*100)])
-print(tabulate(table, headers = ['solver', 'multiclass', 'Precision', 'Exactitud']))       
-pd.write_csv()
+        for c in C:
+            if mc=='multinomial':
+                if s=='liblinear':
+                    break
+            lr = LogisticRegression(C=c, random_state =1,solver=s,multi_class=mc)
+            lr.fit(X_train, y_train)
+            y_pred = lr.predict(X_test)
+            #Verificacion
+            table.append([c,s,mc,"{:.4f}".format(precision_score(y_test, y_pred)*100),"{:.4f}".format(accuracy_score(y_test,y_pred)*100)])
+            #plot_decision_regions(X_train,y_train,classifier=lr )
+print(tabulate(table, headers = ['C','solver', ' multiclass', ' Precision', ' Exactitud'], tablefmt="github"))
+print('-------------------------------------------------')       
---
+++ Tarea 1 LogisticRegression/Tarea_Logistic_Regression_2.py
@@ -0,0 +1,207 @@
+import numpy as np
+from sklearn import datasets
+
+cancer = datasets.load_iris()
+X = cancer.data[:,[2,3]]
+y = cancer.target
+print('----------------------------------------')
+print('Class labels:', np.unique(y))
+print('----------------------------------------')
+#Imprime [0 1 2] que son iris-setosa, iris-versicolor y iris-virginca
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3, random_state=1, stratify=y)
+
+print('Labels counts in y      : ', np.bincount(y))
+print('Labels counts in y_train: ', np.bincount(y_train))
+print('Labels counts in y_test : ', np.bincount(y_test))
+print('----------------------------------------')
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(eta0=0.1, random_state=1)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified examples: %d' % (y_test != y_pred).sum())
+print('----------------------------------------')
+
+from sklearn.metrics import accuracy_score
+print('Accuracy : %.3f ' % accuracy_score(y_test,y_pred))
+print('Accuracy : %.3f ' % ppn.score(X_test_std, y_test))
+print('----------------------------------------')
+
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt 
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02) :
+
+    #Setup market generator and colormap
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue','lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y)) ] )
+
+    # plot the decision surface
+    x1_min, x1_max = X[:,0].min() -1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:,1].min() -1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+
+    #plot class example
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y== cl,0], y = X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl, edgecolor='black')
+    if test_idx:
+        #plot all examples
+        X_test, y_test, = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:,1], c='', edgecolor='black', alpha=1.0, linewidth=1, marker='o',s=100 ,label='test set')
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std,y=y_combined,classifier=ppn,test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# Clase Regresion Logistica
+class LogisticRegressionGD(object):
+    """Logistic Regression Classifier using gradient descent.
+    Parameters
+    ------------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    random_state : int
+        Random number generator seed for random weight
+        initialization.
+    Attributes
+    -----------
+    w_ : 1d-array
+        Weights after fitting.
+    cost_ : list
+        Logistic cost function value in each epoch.
+    """
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    def fit(self, X, y):
+        """ Fit training data.
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+        Training vectors, where n_examples is the number of
+        examples and n_features is the number of features.
+        y : array-like, shape = [n_examples]
+        Target values.
+        Returns
+        -------
+        self : object
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01,
+                              size=1 + X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            # note that we compute the logistic 'cost' now
+            # instead of the sum of squared errors cost
+            cost = (-y.dot(np.log(output)) -
+            ((1 - y).dot(np.log(1 - output))))
+            self.cost_.append(cost)
+        return self
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X))
+        # >= 0.5, 1, 0)
+
+X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+lrgd = LogisticRegressionGD(eta=0.05, n_iter=1000, random_state=1)
+lrgd.fit(X_train_01_subset, y_train_01_subset)
+plot_decision_regions(X=X_train_01_subset, y=y_train_01_subset, classifier=lrgd)
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#SIGMOIDE
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+# y axis ticks and gridline
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+plt.tight_layout()
+plt.show()
+
+#FUNCION DE COSTE
+def cost_1(z):
+    return - np.log(sigmoid(z))
+def cost_0(z):
+    return - np.log(1 - sigmoid(z))
+z = np.arange(-10, 10, 0.1)
+phi_z = sigmoid(z)
+c1 = [cost_1(x) for x in z]
+plt.plot(phi_z, c1, label='J(w) if y=1')
+c0 = [cost_0(x) for x in z]
+plt.plot(phi_z, c0, linestyle='--', label='J(w) if y=0')
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+plt.xlabel('$\phi$(z)')
+plt.ylabel('J(w)')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C=100.0, random_state=1,solver='lbfgs', multi_class='ovr')
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,y_combined,classifier=lr,test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+lr.predict_proba(X_test_std[:3,:])
+lr.predict_proba(X_test_std[:3,:]).argmax(axis=1)
+lr.predict(X_test_std[:3,:])
+lr.predict(X_test_std[0,:].reshape(1,-1))
